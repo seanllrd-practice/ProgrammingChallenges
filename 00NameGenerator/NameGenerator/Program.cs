49e29aeabd230135fe900305ef15bc26a688600c
Sean Ellard <seanllrd5@gmail.com>
2020-06-10 22:38:41 -0400

    First commit

---
+++ 00NameGenerator/NameGenerator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace NameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = GetNameLength();
+            
+            string result = RandomName(Convert.ToInt32(length));
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        static int GetNameLength()
+        {
+            int number;
+
+            while (true)
+            {
+                Console.Write("Enter length of username: ");
+                string input = Console.ReadLine();
+
+                if (!Int32.TryParse(input, out number))
+                {
+                    Console.WriteLine("Invalid input. Please enter a number only.");
+                    continue;
+                }
+
+                break;
+            }
+
+            return number;
+        }
+
+        static string RandomName(int count)
+        {
+            char[] letters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            string result = "";
+            Random random = new Random();
+            TextInfo mTI = new CultureInfo("en-US", false).TextInfo;
+
+            for (int i = 0; i < count; i++)
+            {
+                int randomLetter = random.Next(letters.Length);
+                result += letters.GetValue(randomLetter);
+            }
+
+            result = mTI.ToTitleCase(result);
+            return result;
+        }
+    }
+}
